5a79b0374a65328a7fd7f680b8074faac7462396
Mehmet Ugur SATIR <musatir@gmail.com>
2022-06-07 00:33:50 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const addTask = document.querySelector('.add-task');
+const tasks = document.querySelector('.tasks');
+const input = document.querySelector('.input-group input');
+const todo = document.getElementById('todo');
+const checks = document.querySelectorAll('.tasks input')
+// this part of code is for adding new tasks
+addTask.addEventListener('click', taskAdder);
+todo.addEventListener('keydown', (event)=>{
+    if (event.key == 'Enter'){
+        taskAdder();
+    }
+})
+
+function taskAdder(){
+    tasks.innerHTML += `<div class="task"><div class="left"><input class = "check" type="checkbox" name="" id=""><p>${todo.value}</p></div><i class="fa-solid fa-trash-can"></i></div>`
+    todo.value = '';
+}
+
+// this part of code is for returning feedback about the number of completed tasks
+tasks.addEventListener('click', (event) =>{
+   if (event.target.classList.contains('check')){
+       event.target.parentElement.classList.toggle('strikethrough');
+   } else if (event.target.classList.contains('fa-trash-can')){
+    event.target.parentElement.remove();
+   }
+   let completed = 0;
+   let rest = 0;
+   document.querySelectorAll('.check').forEach((a)=>{
+    if (a.parentElement.classList.contains('strikethrough')) {
+        completed++;
+    } else {
+        rest++;
+    }
+})
+document.querySelector('.stats p').innerText = `${completed} OUT OF ${completed+rest} TASKS COMPLETED`
+});
